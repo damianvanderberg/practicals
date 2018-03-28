1307a0c51ad3cbfe5bd8b116f6c3c58658af23c3
damianvanderberg <damian.vanderberg@my.jcu.edu.au>
2018-03-28 11:04:57 +1000

    list comprehension

--- Prac_4/monthly_income.py
+++ Prac_4/monthly_income.py
@@ -5,20 +5,25 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of monthly_."""
+    """Display income report for incomes over a given number of how_many_months."""
     incomes = []
-    monthly_ = int(input("How many monthly_? "))
+    how_many_months = int(input("How many months? "))
 
-    for monthly_ in range(1, monthly_ + 1):
-        income = float(input("Enter income for monthly_ {} :".format(monthly_)))
+    for month in range(1, how_many_months + 1):
+        income = float(input("Enter income for month {} :".format(month)))
         incomes.append(income)
 
+    income_report(incomes)
+
+
+def income_report(incomes):
+    """"report indicating incomes and running total after each month"""
+
     print("\nIncome Report\n-------------")
-    total = 0
-    for mon in range(1, monthly_ + 1):
-        income = incomes[monthly_ - 1]#why -1
+    total = 0  # running total of income after inputs
+    for month, income in enumerate(incomes): #enumerate adds a counter for each iterate in the brackets (income)
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(monthly_, income, total))
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month+1, income, total))
 
 
 main()
---
+++ Prac_4/woefully_inadequite_checker.py
@@ -0,0 +1,17 @@
+
+#ask for username
+#check if username is in above usernames
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye',
+             'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command',
+             'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+             'StartServer', 'bob']
+
+username = input("Enter username: ")
+
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
+
+    
